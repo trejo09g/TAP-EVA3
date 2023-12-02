0293246e4e9c43b0d542a581789cb2f3fca63dd2
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-12-01 23:12:54 -0600

    Add files via upload

---
+++ EVA3_1_PRACTICA/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
